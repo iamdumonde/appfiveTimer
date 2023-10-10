627b75106565f37e583e175fcdc0935e2185b441
iamdumonde <martinaboumonde@gmail.com>
2023-10-10 14:39:37 +0100

    Simple changes

--- resources/js/Components/Timer.vue
+++ resources/js/Components/Timer.vue
@@ -1,80 +1,45 @@
 <script setup>
-// import { reactive } from "vue";
-
-// const props = defineProps(['quiz']);
-
-// let duration = parseInt(props.quiz.duration);
-// let state = reactive({
-//     min: duration - 1,
-//     secondes: 59,
-//     cent: 99,
-//     timeoutID: "",
-// });
-
-// function startQuizz() {
-//     state.timeoutID = setTimeout(() => {
-//         if (state.min > -1) {
-//             state.cent--;
-//             if (state.cent === 0) {
-//                 state.cent = 99;
-//                 state.secondes--;
-//             } else if (state.secondes === 0) {
-//                 state.secondes = 60;
-//                 state.min--;
-//             }
-//         } else if (state.min <= -1) {
-//             clearTimeout(state.timeoutID);
-//             state.min = 0;
-//             state.cent = 0;
-//             state.secondes = 0;
-//             // console.log('clearInterval');
-//         }
-//         startQuizz();
-//     }, 0.1);
-// }
-
-// function doubleNum(number) {
-//     if (number < 10) {
-//         return '0' + number;
-//     } else {
-//         return number;
-//     }
-// }
-
-// ==============================================================
-import { onBeforeUnmount, onMounted, reactive } from 'vue';
+import { reactive } from "vue";
 
 const props = defineProps(['quiz']);
 
 let duration = parseInt(props.quiz.duration);
-
 let state = reactive({
     min: duration - 1,
     secondes: 59,
     cent: 99,
+    intervalID: "",
 });
 
-let timerWorker = new Worker('./timerWorker.js');
-
-timerWorker.onmessage = function(event){
-    state.value = event.data;
+function startQuizz() {
+    state.intervalID = setInterval(() => {
+        if (state.min > -1) {
+            state.cent--;
+            if (state.cent === 0) {
+                state.cent = 99;
+                state.secondes--;
+            } else if (state.secondes === 0) {
+                state.secondes = 60;
+                state.min--;
+            }
+        } else if (state.min <= -1) {
+            clearInterval(state.intervalID);
+            state.min = 0;
+            state.cent = 0;
+            state.secondes = 0;
+        }
+    }, 10);
 }
 
-onMounted(() => {
-    timerWorker.postMessage('start');
-});
-
-onBeforeUnmount(() => {
-    timerWorker.postMessage('start');
-});
-
-function doubleNum(number){
+function doubleNum(number) {
     if (number < 10) {
         return '0' + number;
     } else {
         return number;
     }
 }
+
+
 </script>
 
 <!-- <style scoped>
@@ -108,7 +73,7 @@ function doubleNum(number){
                 </span>
             </div>
 
-            <button @click="startTimer" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Chrono</button>
+            <button @click="startQuizz" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Chrono</button>
         </div>
 
     </div>
