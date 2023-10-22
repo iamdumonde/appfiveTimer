1d4bf5c8d4393c8cd83ec9ee67e77a39954f9988
iamdumonde <128897874+iamdumonde@users.noreply.github.com>
2023-10-22 20:06:02 +0100

    pas trouvé

--- resources/js/Components/Timer.vue
+++ resources/js/Components/Timer.vue
@@ -1,80 +1,60 @@
 <script setup>
-// import { reactive } from "vue";
-
-// const props = defineProps(['quiz']);
-
-// let duration = parseInt(props.quiz.duration);
-// let state = reactive({
-//     min: duration - 1,
-//     secondes: 59,
-//     cent: 99,
-//     timeoutID: "",
-// });
-
-// function startQuizz() {
-//     state.timeoutID = setTimeout(() => {
-//         if (state.min > -1) {
-//             state.cent--;
-//             if (state.cent === 0) {
-//                 state.cent = 99;
-//                 state.secondes--;
-//             } else if (state.secondes === 0) {
-//                 state.secondes = 60;
-//                 state.min--;
-//             }
-//         } else if (state.min <= -1) {
-//             clearTimeout(state.timeoutID);
-//             state.min = 0;
-//             state.cent = 0;
-//             state.secondes = 0;
-//             // console.log('clearInterval');
-//         }
-//         startQuizz();
-//     }, 0.1);
-// }
-
-// function doubleNum(number) {
-//     if (number < 10) {
-//         return '0' + number;
-//     } else {
-//         return number;
-//     }
-// }
-
-// ==============================================================
-import { onBeforeUnmount, onMounted, reactive } from 'vue';
+import { onMounted } from 'vue';
+import { reactive } from "vue";
 
 const props = defineProps(['quiz']);
-
 let duration = parseInt(props.quiz.duration);
-
 let state = reactive({
-    min: duration - 1,
-    secondes: 59,
-    cent: 99,
+    inputRangeValue: 0, // Démarre à 0
+    min: duration ,
+    secondes: 0,
+    cent: 0,
+    timeoutID: null,
 });
 
-let timerWorker = new Worker('./timerWorker.js');
-
-timerWorker.onmessage = function(event){
-    state.value = event.data;
+function startQuizz() {
+    if (state.timeoutID) return; // Évite de lancer plusieurs minuteries
+    state.timeoutID = setInterval(() => {
+        if (state.min > 0 || (state.secondes > 0 || state.cent > 0)) {
+            if (state.cent === 0) {
+                state.cent = 99;
+                if (state.secondes === 0) {
+                    state.secondes = 59;
+                    state.min--;
+                } else {
+                    state.secondes--;
+                }
+                state.inputRangeValue = 100 - (state.min * 60 + state.secondes) / (duration * 60) * 100;
+            } else {
+                state.cent--;
+            }
+        } else {
+            clearInterval(state.timeoutID);
+            state.timeoutID = null;
+        }
+    }, 10);
 }
 
-onMounted(() => {
-    timerWorker.postMessage('start');
-});
-
-onBeforeUnmount(() => {
-    timerWorker.postMessage('start');
-});
-
-function doubleNum(number){
+function doubleNum(number) {
     if (number < 10) {
         return '0' + number;
     } else {
         return number;
     }
 }
+
+// onMounted(() => {
+//   const inputRange = document.getElementById('range');
+//   console.log(inputRange);
+//   inputRange.addEventListener('click', () => {
+//     const pourcentage = (inputRange.value - inputRange.min) / (inputRange.max - inputRange.min);
+//     const couleur = `linear-gradient(to right, red ${pourcentage * 100}%, #ccc ${pourcentage * 100}%)`;
+    
+//     inputRange.style.background = couleur;
+//   });
+// });
+
+
 </script>
 
 <template>
@@ -95,8 +75,32 @@ function doubleNum(number){
                 </span>
             </div>
 
-            <button @click="startTimer" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Chrono</button>
+            <button @click="startQuizz" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Chrono</button>
+            <input type="range" name="" id="range"
+            v-model="state.inputRangeValue"
+            class="w-full custom-input-range"
+            >
+            {{ state.inputRangeValue }}
         </div>
 
     </div>
 </template>
+
+<style>
+input[type=range] {
+    pointer-events: none;
+}
+
+/* Change la couleur de la piste lors du déplacement du slider */
+/* Ajoute un dégradé si vous le souhaitez */
+/* input[type="range"]::-webkit-slider-runnable-track {
+    background: linear-gradient(to right, yellow, gold);
+} */
+
+/* Pour Firefox */
+/* input[type="range"]::-moz-range-track {
+    background: linear-gradient(to right, #votre_couleur, #votre_couleur);
+} */
+
+
+</style>
